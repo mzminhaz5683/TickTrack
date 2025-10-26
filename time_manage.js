ff123b2f7f87ba6ffc94e0a5968b23bfb9c38d4d
Minhazul Zannat (Minhaz) <minhaz@metaii.com>
2025-10-26 21:27:47 +0600

    TT-01: update timer

--- time_manage.js
+++ time_manage.js
@@ -1,9 +1,9 @@
 const timers = {
   'jumper': {
-    'work_time_in_minutes': 0,
+    'work_time_in_minutes': 1,
     'relax_time_in_minutes': 0,
-    'work_time_in_seconds': 3,
-    'relax_time_in_seconds': 2,
+    'work_time_in_seconds': 0,
+    'relax_time_in_seconds': 30,
     'round': 2,
   },
 };
